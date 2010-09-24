6fc2e76ef9e9441cea70ba58cf17b5ec16f8c06b
Vytis Valentinavičius <xytis2000@gmail.com>
2010-09-24 19:20:48 +0300

    Replaced the Interface class

---
+++ include/gui/cInterface.cpp
@@ -0,0 +1,89 @@
+#include "cInterface.h"
+
+
+namespace DoI
+{
+    cInterface::
+    cInterface(QWidget *parent):
+    app(NULL)
+    {
+        setupUi(this); // this sets up GUI
+
+        // signals/slots mechanism in action
+        connect( pushButton_browse, SIGNAL( clicked() ), this, SLOT( getPath() ) );
+        connect( pushButton_run, SIGNAL( clicked() ), this, SLOT( doSomething() ) );
+        connect( pushButton_clear, SIGNAL( clicked() ), this, SLOT( clear() ) );
+        connect( pushButton_about, SIGNAL( clicked() ), this, SLOT( about() ) );
+    }
+
+    void cInterface::
+    connect(cMaterial * object)
+    {
+        m_object = object;
+    }
+
+
+
+
+
+
+
+    QString cInterface::
+    getPath()
+    {
+        QString path;
+
+        path = QFileDialog::getOpenFileName(
+            this,
+            "Choose a constants or dump file to open",
+            QString::null,
+            QString::null);
+
+        lineEdit->setText( path );
+        return path;
+    }
+
+
+    void cInterface::
+    doSomething()
+    {
+        int value1, value2;
+        Qt::CheckState state;
+        QString str;
+
+        textEdit->append( "Path to file: " + lineEdit->text() );
+
+        value1 = spinBox1->value();
+        value2 = spinBox2->value();
+
+        textEdit->append( "Number 1 value: " + QString::number(value1) );
+        textEdit->append( "Number 2 value: " + QString::number(value2) );
+
+        state = checkBox->checkState();
+
+        str = "Checkbox says: ";
+        if ( state == Qt::Checked ) str += "yes";
+        else str += "no";
+        textEdit->append( str );
+
+        textEdit->append( "ComboBox current text: " + comboBox->currentText() );
+        textEdit->append( "ComboBox current item: " + QString::number(comboBox->currentIndex()) );
+    }
+
+
+    void cInterface::
+    clear()
+    {
+        textEdit->clear();
+    }
+
+
+    void cInterface::
+    about()
+    {
+        QMessageBox::about(this,"About XSim",
+                    "This app was coded for educational purposes.\n"
+                    "Number 1 is: " + QString::number(spinBox1->value()) + "\n\n"
+                    "Bye.\n");
+    }
+};
---
+++ include/gui/cInterface.h
@@ -0,0 +1,55 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  cInterface.h
+ *
+ *    Description:  Class which draws the pretty pictures =]
+ *
+ *        Version:  2.0
+ *        Created:  2010-09-14-22.04
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Vytis Valentinavičius (), vytis.valentinavicius@gmail.com
+ *        Company:  VU FF
+ *
+ * =====================================================================================
+ */
+#ifndef CINTERFACE_H_INCLUDED
+#define CINTERFACE_H_INCLUDED
+
+#include "../cMaterial.h"
+
+#include "ui_gui.h"
+#include <QtGui>
+#include <QApplication>
+
+
+namespace DoI
+{
+    class cInterface : public QWidget, private Ui::Form
+    {
+        Q_OBJECT
+
+        private:
+            QApplication * app;
+
+            cMaterial * m_object;
+
+
+        public:
+            cInterface(QWidget *parent = 0);
+            void connect(cMaterial *);
+            void init(int *, char ***);
+            int lounch();
+
+
+        public slots:
+            QString getPath();
+            void doSomething();
+            void clear();
+            void about();
+    };
+};
+
+#endif // CINTERFACE_H_INCLUDED
