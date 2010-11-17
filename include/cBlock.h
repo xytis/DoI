fa038670fcfa9e78424a5caea703d7978cb63d1d
Vytis Valentinavičius <xytis2000@gmail.com>
2010-11-17 11:44:29 +0200

    Moved the cField out of cBlock.h

--- include/cBlock.h
+++ include/cBlock.h
@@ -23,6 +23,7 @@
 
 #include "global.h"
 #include "cData.h"
+#include "cField.hpp"
 #include "cStatus.h"
 #include "cConstants.h"
 
@@ -36,42 +37,8 @@ namespace DoI
 
     class cData;
     class IBlock;
+    class cField;
 
-    class cField
-    {
-        private:
-            cField * m_next;
-            cField * m_prev;
-            IBlock * m_left;
-            IBlock * m_right;
-        public:
-            cField();
-
-            cField * next();
-            cField * prev();
-
-            void set_next(cField *);
-            void set_prev(cField *);
-            void attach_left(IBlock *);
-            void attach_right(IBlock *);
-
-
-            friend std::ostream & operator << (std::ostream &, cField &);
-            void iprint(std::ostream &); //Saviiteruojantis metodas.
-
-            double calculate(double); //Saviiteruojantis metodas
-            double integrate();
-            void force_raise(double);
-
-            //Tarpinis kintamasis dE
-            double dE;
-
-            double supE;
-            double infE;
-
-            //Kintamasis laukui saugoti
-            double E;
-    };
 
     class IBlock
     {
@@ -113,7 +80,7 @@ namespace DoI
             cField * m_E_next;
             cField * m_E_prev;
             cData    m_data;
-            //MOVED BUFER AREA TO m_data
+            //MOVED BUFFER AREA TO m_data
             //double   m_n_buffer;
             //double   m_p_buffer;
 
---
+++ include/cField.hpp
@@ -0,0 +1,45 @@
+#ifndef CFIELD_HPP_INCLUDED
+#define CFIELD_HPP_INCLUDED
+
+#include "cBlock.h"
+
+namespace DoI
+{
+    class cField
+    {
+        private:
+            cField * m_next;
+            cField * m_prev;
+            IBlock * m_left;
+            IBlock * m_right;
+        public:
+            cField();
+
+            cField * next();
+            cField * prev();
+
+            void set_next(cField *);
+            void set_prev(cField *);
+            void attach_left(IBlock *);
+            void attach_right(IBlock *);
+
+
+            friend std::ostream & operator << (std::ostream &, cField &);
+            void iprint(std::ostream &); //Saviiteruojantis metodas.
+
+            double calculate(double); //Saviiteruojantis metodas
+            double integrate();
+            void force_raise(double);
+
+            //Tarpinis kintamasis dE
+            double dE;
+
+            double supE;
+            double infE;
+
+            //Kintamasis laukui saugoti
+            double E;
+    };
+};
+
+#endif //CFIELD_HPP_INCLUDED
